93ee885cf9e2a58c2b0dec8135939eddd9025c93
Robyn MacDonald <Robyn.MacDonald00@gmail.com>
2024-06-30 16:32:02 -0500

    amenities list

--- src/routes/amenities/+page.svelte
+++ src/routes/amenities/+page.svelte
@@ -1,1 +1,21 @@
+<script lang="ts">
+	const amenitiesList = [
+		'Candy and ice cream store',
+		'Family fun',
+		'Sunbathing/swimming dock',
+		'​Free Wi-Fi',
+		'Fully equipped kitchen',
+		'Ping-Pong',
+		'Air hockey and foosball',
+		'Volleyball',
+		'Games and books for check out',
+		'Canoe and paddle boat usage'
+	];
+</script>
+
 <h1>Amenities</h1>
+<ul>
+    {#each amenitiesList as amenity}
+    <li>{amenity}</li>
+    {/each}
+</ul>
