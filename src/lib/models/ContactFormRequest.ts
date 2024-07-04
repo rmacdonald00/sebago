8a17885dc7b085968ffc50656858e92c1254fbff
Robyn MacDonald <Robyn.MacDonald00@gmail.com>
2024-07-04 10:55:42 -0500

    add a contact form

---
+++ src/lib/models/ContactFormRequest.ts
@@ -0,0 +1,6 @@
+export interface ContactFormRequest {
+    name: string;
+    email: string;
+    subject: string;
+    message: string;
+}
--- src/routes/contact-us/+page.svelte
+++ src/routes/contact-us/+page.svelte
@@ -1,1 +1,44 @@
+<script lang="ts">
+	import type { ContactFormRequest } from '$lib/models/ContactFormRequest';
+
+	const contactFormRequest: ContactFormRequest = {
+		name: '',
+		email: '',
+		subject: '',
+		message: ''
+	};
+
+	const submitContactForm = () => {
+		console.log(contactFormRequest);
+		//TODO: actually send this somewhere
+	};
+</script>
+
 <h1>Contact Us</h1>
+<form on:submit={submitContactForm}>
+	<input
+		id="name"
+		type="text"
+		maxlength="255"
+		bind:value={contactFormRequest.name}
+		placeholder="Name"
+		required
+	/>
+	<input
+		id="email"
+		type="email"
+		bind:value={contactFormRequest.email}
+		placeholder="Email"
+		required
+	/>
+	<input
+		id="subject"
+		type="text"
+		maxlength="255"
+		bind:value={contactFormRequest.subject}
+		placeholder="Subject"
+	/>
+	<textarea id="message" bind:value={contactFormRequest.message} placeholder="Message" />
+
+	<button type="submit">Send</button>
+</form>
