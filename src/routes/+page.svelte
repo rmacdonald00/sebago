ce2f3888afdd3280c641a34bbb65288fe401e9a4
Robyn MacDonald <Robyn.MacDonald00@gmail.com>
2024-07-04 19:29:54 -0500

    add dock

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,13 +8,14 @@
 <span>on Round Lake</span>
 <br />
 <img src="./store-front.webp" alt="Outside of the Candy Store." class={'image'} />
+<img src="./dock.webp" alt="Outside of the Candy Store." class={'image'} />
 
-<span
-	>Sebago Resort is located in Nisswa, Minnesota on the north side of Round Lake, part of the Gull
+<div>
+	Sebago Resort is located in Nisswa, Minnesota on the north side of Round Lake, part of the Gull
 	Lake Chain in north-central Minnesota. Built in 1926 we have sought to keep Sebago simple and
 	affordable and as timeless as possible. We have 6 cottages available for weekly rentals.* Our
 	motto a “time and a place” has meant a week of togetherness, fun and relaxation.
-</span>
+</div>
 <span> * when available cabins may be rented for a 3 night minimum</span>
 
 <ListDisplay title={'Amenities'} items={AmenitiesList} />
