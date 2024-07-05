9780dc116b4c11d4554aa1f17676eb882dd3fca7
Robyn MacDonald <Robyn.MacDonald00@gmail.com>
2024-07-04 19:23:13 -0500

    contact form formatting

--- src/routes/contact-us/+page.svelte
+++ src/routes/contact-us/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { SebagoDetails } from '$lib/data/WebsiteContent';
 	import type { ContactFormRequest } from '$lib/models/ContactFormRequest';
 
 	const contactFormRequest: ContactFormRequest = {
@@ -16,29 +17,58 @@
 
 <h1>Contact Us</h1>
 <form on:submit={submitContactForm}>
-	<input
-		id="name"
-		type="text"
-		maxlength="255"
-		bind:value={contactFormRequest.name}
-		placeholder="Name*"
-		required
-	/>
-	<input
-		id="email"
-		type="email"
-		bind:value={contactFormRequest.email}
-		placeholder="Email*"
-		required
-	/>
-	<input
-		id="subject"
-		type="text"
-		maxlength="255"
-		bind:value={contactFormRequest.subject}
-		placeholder="Subject"
-	/>
-	<textarea id="message" bind:value={contactFormRequest.message} placeholder="Message" />
-
-	<button type="submit">Send</button>
+	<div class={'form-inputs'}>
+		<div class={'left-col'}>
+			<input
+				id="name"
+				type="text"
+				maxlength="255"
+				bind:value={contactFormRequest.name}
+				placeholder="Name*"
+				required
+			/>
+			<input
+				id="email"
+				type="email"
+				bind:value={contactFormRequest.email}
+				placeholder="Email*"
+				required
+			/>
+			<input
+				id="subject"
+				type="text"
+				maxlength="255"
+				bind:value={contactFormRequest.subject}
+				placeholder="Subject"
+			/>
+		</div>
+		<textarea
+			id="message"
+			bind:value={contactFormRequest.message}
+			placeholder="Message"
+			class={'message-input'}
+		/>
+		<button type="submit">Send</button>
+	</div>
 </form>
+
+<div>
+	You can also call or email us directly at {SebagoDetails.phoneNumber} or {SebagoDetails.email}
+</div>
+
+<style>
+	.form-inputs {
+		display: grid;
+		width: 100%;
+		grid-template-columns: 3fr 3fr 1fr;
+	}
+
+	.left-col {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.message-input {
+		resize: none;
+	}
+</style>
