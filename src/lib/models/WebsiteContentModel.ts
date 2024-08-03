0d05dafdd34963b57d9e353dc7f7d325c49a540b
Robyn MacDonald <Robyn.MacDonald00@gmail.com>
2024-08-03 11:20:58 -0500

    idk, some changes toward figuring it out

---
+++ src/lib/models/WebsiteContentModel.ts
@@ -0,0 +1,44 @@
+import type { CabinDetails } from "$lib/models/CabinDetails";
+
+export const CurrentModelVersion = 1;
+
+export interface WebsiteContent extends Document {
+    ModelVersionKey: number;
+    SebagoContactDetails: SebagoInfo;
+    AboutSections: string[];
+    RentalTerms: Terms;
+    CabinDetails: CabinDetails[];
+    HomePageRows: HomePageRow[];
+}
+
+export interface SebagoInfo {
+    phoneNumber: string;
+    address: Address;
+    email: string;
+    facebook: SocialMedia;
+    instagram: SocialMedia;
+}
+
+export interface Address {
+    street: string;
+    city: string;
+    state: string;
+    zip: string;
+    directions: string;
+}
+
+interface SocialMedia {
+    username: string;
+    externalLink: string;
+}
+
+export interface Terms {
+    checkInTime: string;
+    checkOutTime: string;
+    bookingTermsDescription: string;
+}
+
+export interface HomePageRow {
+    title: string;
+    items: string[];
+}
--- src/lib/server/database/WebsiteContentCollection.ts
+++ src/lib/server/database/WebsiteContentCollection.ts
@@ -1,15 +1,18 @@
-import type { WebsiteContent } from "$lib/models/WebsiteContent";
-import type { Sort } from "mongodb";
+import type { Sort, WithId } from "mongodb";
 import ICollection from "./ICollection";
+import { CurrentModelVersion, type WebsiteContent } from "../../models/WebsiteContentModel";
 
 export default class WebsiteContentCollection extends ICollection<WebsiteContent> {
     protected collectionName = 'website-content-versions';
 
-    public async getLatestEntry() {
+    public async getLatestEntry(): Promise<WithId<WebsiteContent> | null> {
         const collection = await this.getCollection();
+        const filter = {
+            ModelVersionKey: { $eq: CurrentModelVersion }
+        }
         const sort: Sort = {
             _id: -1
         };
-        return collection.findOne({}, { sort: sort });
+        return collection.findOne(filter, { sort: sort });
     }
 }
